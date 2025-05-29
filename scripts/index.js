dbbc81756f65bb2bd55bb4937cb869940764987a
Daniil Yakimkin <daniil666n@yandex.ru>
2025-05-29 16:53:42 +0300

    Добавлена конфигурация npm и Webpack

--- scripts/index.js
+++ scripts/index.js
@@ -45,18 +45,21 @@ const profilePopup = document.querySelector('.popup_type_edit')
 const cardPopup = document.querySelector('.popup_type_new-card')
 const imagePopup = document.querySelector('.popup_type_image')
 
-function openModal(popup) {      
+function openModal(popup) {
     popup.classList.add('popup_is-opened');
+    document.addEventListener('keydown', handleEscapeKey);
 }
 
 function closeModal(popup) {
-    popup.classList.remove('popup_is-opened')
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', handleEscapeKey);
 }
 
-const allPopups = document.querySelectorAll('.popup')
+const allPopups = document.querySelectorAll('.popup');
 allPopups.forEach(popup => {
-    popup.classList.add('popup_is-animated')
-})
+    popup.addEventListener('mousedown', handleOverlayClick);
+    popup.classList.add('popup_is-animated');
+});
 
 imagePopup.querySelector('.popup__close').addEventListener('click', () => closeModal(imagePopup))
 
@@ -150,3 +153,97 @@ function handleCardFormSubmit(evt) {
 }
 
 cardFormElement.addEventListener('submit', handleCardFormSubmit);
+
+// 2.1 Валидация формы «Редактировать профиль»
+const nameError = profilePopup.querySelector('.popup__error_type_name');
+const jobError = profilePopup.querySelector('.popup__error_type_description');
+const saveButton = profilePopup.querySelector('.popup__button');
+
+function checkFormValidity() {
+    const isFormValid = profileFormElement.checkValidity();
+    saveButton.disabled = !isFormValid;
+}
+
+nameInput.addEventListener('input', () => {
+    if (nameInput.validity.valid) {
+        nameError.textContent = '';
+    } else {
+        nameError.textContent = nameInput.validationMessage;
+    }
+    checkFormValidity();
+});
+
+jobInput.addEventListener('input', () => {
+    if (jobInput.validity.valid) {
+        jobError.textContent = '';
+    } else {
+        jobError.textContent = jobInput.validationMessage;
+    }
+    checkFormValidity();
+});
+
+checkFormValidity();
+
+// 2.2 Валидация формы «Новое место»
+const placeNameInput = cardPopup.querySelector('.popup__input_type_card-name');
+const linkInput = cardPopup.querySelector('.popup__input_type_url');
+const placeNameError = cardPopup.querySelector('.popup__error_type_card-name');
+const linkError = cardPopup.querySelector('.popup__error_type_url');
+const saveCardButton = cardPopup.querySelector('.popup__button');
+
+function checkCardFormValidity() {
+    const isPlaceNameValid = placeNameInput.validity.valid;
+    const isLinkValid = linkInput.validity.valid;
+    
+    saveCardButton.disabled = !(isPlaceNameValid && isLinkValid);
+}
+
+placeNameInput.addEventListener('input', () => {
+    if (placeNameInput.validity.valid) {
+        placeNameError.textContent = '';
+    } else {
+        if (placeNameInput.validity.tooShort || placeNameInput.validity.tooLong) {
+            placeNameError.textContent = 'Длина должна быть от 2 до 30 символов';
+        } else if (placeNameInput.validity.valueMissing) {
+            placeNameError.textContent = 'Вы пропустили это поле.';
+        }
+    }
+    checkCardFormValidity();
+});
+
+linkInput.addEventListener('input', () => {
+    if (linkInput.validity.valid) {
+        linkError.textContent = '';
+    } else {
+        if (linkInput.validity.typeMismatch) {
+            linkError.textContent = 'Введите адрес сайта.';
+        } else if (linkInput.validity.valueMissing) {
+            linkError.textContent = 'Вы пропустили это поле.';
+        }
+    }
+    checkCardFormValidity();
+});
+
+checkCardFormValidity();
+
+// 2.3 Закрытие поп-апа кликом на оверлей
+function isClickOnOverlay(evt) {
+    return evt.target === evt.currentTarget;
+}
+
+function handleOverlayClick(evt) {
+    if (isClickOnOverlay(evt)) {
+        const popup = evt.currentTarget;
+        closeModal(popup);
+    }
+}
+
+// 2.4 Закрытие по-папа нажатием на Esc
+function handleEscapeKey(evt) {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_is-opened');
+        if (openedPopup) {
+            closeModal(openedPopup);
+        }
+    }
+}
